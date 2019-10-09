2161d181fb26b6596b50b2e1c6eeaecd9aaee0eb
kkallakkum <dkswns333@agilesoda.ai>
2019-10-09 22:59:13 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='kidi_demo',
+    version='0.1',
+    packages=['scratch_mlp'],
+    url='https://github.com/kkallakkum/kidi_demo',
+    license='',
+    author='Lee',
+    author_email='',
+    description=''
+)
